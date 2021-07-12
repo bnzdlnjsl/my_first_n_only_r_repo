99981ae531e19ef37e600f7416650025537084c0
bnzdlnjsl <huangxubin.include@gmail.com>
2021-07-12 17:18:25 +0800

    完成第二次作业

--- test.R
+++ test.R
@@ -1,3 +1,4 @@
-tmp <- 3
-gender_neww <- c(f = tmp)
-gender_neww
+test <- data.frame(名字 = c("A", "B"), 身高 = c(160, 180), 体重 = c(56, 60), 喜欢的食物 = c("apple", "banana"))
+test
+test_long <- test %>% gather(key = 特征, value = 具体, c("身高", "体重", "喜欢的食物"))
+test_long

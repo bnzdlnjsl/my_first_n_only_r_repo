8f8cf19129360773121cb7520c234a9f75f01513
bnzdlnjsl <huangxubin.include@gmail.com>
2021-07-14 04:36:16 +0800

    第三次作业

--- test.R
+++ test.R
@@ -1,4 +1,8 @@
-test <- data.frame(名字 = c("A", "B"), 身高 = c(160, 180), 体重 = c(56, 60), 喜欢的食物 = c("apple", "banana"))
-test
-test_long <- test %>% gather(key = 特征, value = 具体, c("身高", "体重", "喜欢的食物"))
-test_long
+gmp <- read.table("data\\gmp.dat")
+gmp$pop <- round(gmp$gmp/gmp$pcgmp)
+str(gmp)
+
+mse <- function(x){
+  return((x[1] - 2)^2 + x[2]^2 + 2)
+}
+nlm(mse, c(x1 = 200, x2 = 1e10))
